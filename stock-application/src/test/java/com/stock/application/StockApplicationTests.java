23d8f0c4ae4f585a10e68fdd50a844a6638b34e4
KimYeeun99 <kyz0608@naver.com>
2025-04-02 22:35:28 +0900

    init: 빠진 설정 추가

---
+++ stock-application/src/test/java/com/stock/application/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
